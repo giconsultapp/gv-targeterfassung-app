ba65b15178d64aca55a791f455cd75ee13ffbdfd
nastiavynohradova <anastasivynohradova@gmail.com>
2024-08-12 10:50:17 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gv-targeterfassung-app/precache-manifest.d58959cbaf49f95c87690c561495af87.js"
+  "/gv-targeterfassung-app/precache-manifest.7aad7b2c7a5ec5a81069b2bb605bd174.js"
 );
 
 self.addEventListener('message', (event) => {
